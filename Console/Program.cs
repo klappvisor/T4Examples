cce5fc4d4a7a4805a2ad81289da823314ab068f9
klappvisor <klappvisor@gmail.com>
2012-09-16 19:09:45 +0700

    initial

---
+++ Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using T4Examples;
+
+namespace TestConsole {
+    class Program {
+        static void Main(string[] args) {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            
+            var template = new MyRuntimeTemplate();
+            var text = template.TransformText();
+            Console.WriteLine(text);
+
+            Console.ReadLine();
+        }
+    }
+}
